588b9dbc8550a46f63c03d1baa3439ac7fa5f9dc
nadin869 <nady86.86@mail.ru>
2024-01-03 17:28:50 +0300

    Самое начало разработки todoBackend

---
+++ resources.py
@@ -0,0 +1,46 @@
+def print_with_indent(value, indent=0):
+    indentation = "\t" * indent
+    print(f"{indentation}{str(value)}")
+    #print(indentation + str(value))
+
+class Entry:
+    def __init__(self, title, entries=None, parent=None):
+        if entries is None:
+            entries = []
+        self.title = title
+        self.entries = entries
+        self.parent = parent
+
+    def __str__(self):
+        return self.title
+
+    def add_entry(self, entry):
+        self.entries.append(entry)
+        #print(f"Добавили {entry.title}")
+        entry.parent = self
+
+    def print_entries(self, indent=0):
+        print_with_indent(self, indent)
+        for entry in self.entries:
+            entry.print_entries(indent + 1)
+
+new_entry = Entry("Продукты")
+meet = Entry("Мясное")
+molochka = Entry("Молочные")
+
+new_entry.add_entry(meet)
+new_entry.add_entry(molochka)
+
+kolbasa = Entry("колбаса")
+meet.add_entry(kolbasa)
+
+moloko = Entry("Молоко")
+molochka.add_entry(moloko)
+
+salami = Entry('Salami')
+kolbasa.add_entry(salami)
+
+chicken = Entry("Chicken")
+salami.add_entry(chicken)
+
+new_entry.print_entries()
