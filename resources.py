1c1f6a3afdab0f8e031a761064268371c5a9a86c
nadin869 <nady86.86@mail.ru>
2024-01-06 16:56:35 +0300

    Добавление класса EntryManager

--- resources.py
+++ resources.py
@@ -1,5 +1,6 @@
 import json
 from test_json import entry
+import os
 def print_with_indent(value, indent=0):
     indentation = "\t" * indent
     print(f"{indentation}{str(value)}")
@@ -45,6 +46,17 @@ class Entry:
         for entry in self.entries:
             entry.print_entries(indent + 1)
 
+    def save_entry(self, path):
+        data = self.json()
+        with open(os.path.join(path, f'{self.title}.json'), 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False)
+
+    @classmethod
+    def load_entry(cls, filename):
+        with open(filename, 'r') as file:
+            data = json.load(file)
+        return cls.entry_from_json(data)
+
 
 new_entry = Entry.entry_from_json(entry)
 new_entry.print_entries()
@@ -52,6 +64,10 @@ print(new_entry.json())
 
 new_entry1 = Entry.entry_from_json(new_entry.json())
 new_entry1.print_entries()
+new_entry.save_entry('D:\programmingPython\/todoBackend')
+
+loaded_entry = Entry.load_entry('D:\programmingPython\/todoBackend\Продукты.json')
+print(loaded_entry)
 
 
 # new_entry = Entry("Продукты")
